b18fc0fd9b823a88e9b05b012850d5b699cf9d2a
Tiago <xernobyl@hotmail.com>
2017-11-03 02:26:14 +0000

    changed file

---
+++ src/matrix.rs
@@ -0,0 +1,35 @@
+use std::ops::Index;
+
+pub type Precision = f64;
+
+pub struct Matrix {
+	data: Vec<Precision>,
+	columns: usize,
+	rows: usize,
+}
+
+impl Matrix {
+	pub fn new(columns: usize, rows: usize) -> Self {
+		Self {
+			data: vec![0.0; columns * rows],
+			columns: columns,
+			rows: rows,
+		}
+	}
+
+	pub fn get_columns(&self) -> usize {
+		self.columns
+	}
+
+	pub fn get_rows(&self) -> usize {
+		self.rows
+	}
+}
+
+impl Index<usize> for Matrix {
+	type Output = [Precision];
+
+	fn index(&self, i: usize) -> &[Precision] {
+		self.data[i * self.columns .. i * self.columns + self.columns]
+	}
+}
